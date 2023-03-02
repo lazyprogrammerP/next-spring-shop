dbb4e7dc4ca99f7907dd9554d696417ecbfad6df
Parshva Runwal <runwalparshva3@gmail.com>
2023-03-03 03:20:07 +0530

    Handling the new ThrowableException in GlobalDefaultExceptionHandler

--- shop/src/main/java/io/spring/shop/exception/GlobalDefaultExceptionHandler.java
+++ shop/src/main/java/io/spring/shop/exception/GlobalDefaultExceptionHandler.java
@@ -1,8 +1,17 @@
 
 package io.spring.shop.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
 public class GlobalDefaultExceptionHandler {
+
+    @ExceptionHandler(ThrowableException.class)
+    public ResponseEntity<Object> handleExceptions(ThrowableException exception, WebRequest webRequest) {
+        return new ResponseEntity<>(exception, exception.getStatusCode());
+    }
+
 }
